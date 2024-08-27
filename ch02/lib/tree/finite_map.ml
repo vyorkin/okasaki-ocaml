7b471b27b90560a88b7f20352efa6e7e83f81749
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2024-08-27 13:27:41 +0300

    Impl MkFiniteMap

--- ch02/lib/tree/finite_map.ml
+++ ch02/lib/tree/finite_map.ml
@@ -2,23 +2,6 @@ open Core
 open Okasaki_common
 open Unbalanced_set
 
-module MkOrderedPair (K : Ordered.S) (V : Ordered.S) :
-  Ordered.S with type t = K.t * V.t = struct
-  type t = K.t * V.t [@@deriving show { with_path = false }]
-
-  let name = sprintf "%s * %s" K.name V.name
-  let z = (K.z, V.z)
-  let equal (k1, _) (k2, _) = K.(equal k1 k2)
-  let ( = ) (k1, _) (k2, _) = K.(k1 = k2)
-  let ( < ) (k1, _) (k2, _) = K.(k1 < k2)
-  let ( > ) (k1, _) (k2, _) = K.(k1 > k2)
-  let ( <= ) (k1, _) (k2, _) = K.(k1 <= k2)
-  let gen = QCheck.Gen.pair K.gen V.gen
-  let arbitrary = QCheck.make gen
-  let generate n = QCheck.Gen.generate ~n gen
-  let generate1 () = QCheck.Gen.generate1 gen
-end
-
 module type FiniteMap = sig
   type k
   type v
@@ -28,27 +11,62 @@ module type FiniteMap = sig
 
   val empty : t
   val bind : k * v * t -> t
-  val lookup : k * t -> v option
+  val lookup : k * t -> v
 end
 
 module MkFiniteMap (K : Ordered.S) (V : Ordered.S) : FiniteMap = struct
-  module E = MkOrderedPair (K) (V)
-  module S = MkUnbalancedSet (E)
+  type k = K.t [@@deriving eq, show { with_path = false }]
+  type v = V.t [@@deriving eq, show { with_path = false }]
+  type elem = k * v [@@deriving eq, show { with_path = false }]
+  type t = E | T of t * elem * t [@@deriving eq, show { with_path = false }]
 
-  type k = K.t
-  type v = V.t
-  type t = S.t [@@deriving show { with_path = false }]
+  let empty = E
+  let node l e r = T (l, e, r)
 
-  let empty = S.empty
+  exception DuplicateKeyError
+  exception NotFoundError
 
-  let bind (k, v, s) =
-    let e = (k, v) in
-    S.insert (e, s)
+  let bind (k, v, t) =
+    let e : K.t * V.t = (k, v) in
+    let rec insert_node (t, k') =
+      let open K in
+      if k' = k then raise DuplicateKeyError
+      else
+        match t with
+        | E -> T (E, e, E)
+        | T (l, ((k', _) as e'), r) when k <= k' ->
+            T (insert_node (l, k'), e', r)
+        | T (l, ((k', _) as e'), r) -> T (l, e', insert_node (r, k'))
+    in
+    try insert_node (t, K.z) with DuplicateKeyError -> t
 
-  let lookup (_k, _s) =
-    failwith "Seems impossible to reuse UnbalancedSet to implement FiniteMap"
+  let lookup (k, t) =
+    let rec find (t, (k', v)) =
+      let open K in
+      match t with
+      | E -> if k = k' then v else raise NotFoundError
+      | T (l, ((k', _) as e), _) when k <= k' -> find (l, e)
+      | T (_, e, r) -> find (r, e)
+    in
+    find (t, (K.z, V.z))
 
-  let gen = S.gen
+  let gen =
+    let open QCheck.Gen in
+    let aux self = function
+      | 0 -> pure empty
+      | n ->
+          frequency
+            [
+              (1, pure empty);
+              ( 2,
+                map3 node
+                  (self (n / 2))
+                  (QCheck.Gen.pair K.gen V.gen)
+                  (self (n / 2)) );
+            ]
+    in
+    sized @@ fix aux
+
   let arbitrary = QCheck.make gen ~print:show
   let generate n = QCheck.Gen.generate ~n gen
   let generate1 () = QCheck.Gen.generate1 gen
